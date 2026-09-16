4ef6ba6a1dab17a0de22b11a5d2ae93e3901896e
johnconstant99-dev <johnconstant99@gmail.com>
2026-09-16 14:52:59 +0100

    Add 5 licensed photo stickers as compact modules

--- assets/christy/photos-finalize.js
+++ assets/christy/photos-finalize.js
@@ -1,1 +1,1 @@
-window.CHRISTY_PHOTOS=(window._CP||[]).map(function(p){return "data:image/jpeg;base64,"+p.join("");});
+window.CHRISTY_PHOTOS=(window._CP||[]).filter(Boolean).map(function(p){return "data:image/jpeg;base64,"+p.join("");});
